a72b7f41476b34e4d237ae21429f1d3060727bb4
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 19:18:19 +0000

    1.1.3

---
+++ leadpages/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '93b265dd8e9b75c09dba3179c3f1f0aac5969f5e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '93b265dd8e9b75c09dba3179c3f1f0aac5969f5e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
